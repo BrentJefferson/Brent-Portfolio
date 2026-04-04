3c9e9395646df82567267fea0e03623b6b0a2886
Brent Orgen <brent@example.com>
2026-04-04 20:36:55 +0800

    Update

--- src/section/home.jsx
+++ src/section/home.jsx
@@ -13,8 +13,8 @@ export const Home = () =>{
   className="w-full min-w-[200px] max-w-[200px] aspect-square rounded-full mb-5 object-cover"
 />
             <div className="p-5 text-center lg:text-2xl md:text-2xl text-head">
-                <h1 className="text-gray-900 non-darktext text-[250%]">Web Developer</h1>
-                <br></br> Hi, I'm Brent Orgen, a web developer and AI enthusiast. I enjoy solving problems, designing solutions, and building tools that improve efficiency. I'm passionate about exploring automation and the endless possibilities of technology.
+                <h1 className="text-gray-900 non-darktext text-[250%]">Developer</h1>
+                <br></br> Hi, I'm Brent Orgen, Analytical and motivated developer with experience in building websites and custom automation system using python and n8n. familiar with software development concepts, web-based systems, automation, and basic AWS(EC2). Self-driven individual with strong problem-solving skills, adaptability, and a passion for tech. Ready to learn, grow, and work independently or with a team.
             </div>
         </div>
         </>
